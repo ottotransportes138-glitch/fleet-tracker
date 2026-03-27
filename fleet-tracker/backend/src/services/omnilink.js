f44f3574cfc5f8d358e863bb25877bb0a010ea95
ottotransportess138 <ottotransportes138@gmail.com>
2026-03-26 22:18:11 -0300

    usa BuscarUltimoIdPost antes de ObtemEventosCtrl

--- fleet-tracker/backend/src/services/omnilink.js
+++ fleet-tracker/backend/src/services/omnilink.js
@@ -5,6 +5,31 @@ const WSTT_URL = "https://wstt.omnilink.com.br/iasws/iasws.asmx";
 const USER = process.env.OMNILINK_USER;
 const PASS = process.env.OMNILINK_PASSWORD;
 
+async function buscarUltimoId() {
+  const soap = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://microsoft.com/webservices/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <web:BuscarUltimoIdPost>
+         <Usuario>${USER}</Usuario>
+         <Senha>${PASS}</Senha>
+      </web:BuscarUltimoIdPost>
+   </soapenv:Body>
+</soapenv:Envelope>`;
+
+  const { data } = await axios.post(WSTT_URL, soap, {
+    headers: {
+      "Content-Type": "text/xml; charset=utf-8",
+      "SOAPAction": "http://microsoft.com/webservices/BuscarUltimoIdPost"
+    },
+    timeout: 15000,
+  });
+
+  console.log("[OMNILINK] BuscarUltimoIdPost resposta:", data.substring(0, 500));
+  const idctrl = data.match(/<idctrl>(.*?)<\/idctrl>/)?.[1] || "0";
+  console.log("[OMNILINK] idctrl:", idctrl);
+  return idctrl;
+}
+
 async function syncOmnilink() {
   const { rows: vehicles } = await db.query(
     "SELECT id, plate, name, omnilink_id, speed_limit FROM vehicles WHERE active = TRUE"
@@ -15,18 +40,18 @@ async function syncOmnilink() {
   const positions = [];
 
   try {
+    const ultimoId = await buscarUltimoId();
+
     const soap = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wst="http://microsoft.com/webservices/">
    <soapenv:Header/>
    <soapenv:Body>
       <wst:ObtemEventosCtrl>
          <Usuario>${USER}</Usuario>
          <Senha>${PASS}</Senha>
-         <UltimoSequencialCtrl>0</UltimoSequencialCtrl>
+         <UltimoSequencialCtrl>${ultimoId}</UltimoSequencialCtrl>
       </wst:ObtemEventosCtrl>
    </soapenv:Body>
 </soapenv:Envelope>`;
-
-    console.log("[OMNILINK] Enviando ObtemEventosCtrl...");
 
     const { data } = await axios.post(WSTT_URL, soap, {
       headers: {
@@ -36,7 +61,7 @@ async function syncOmnilink() {
       timeout: 15000,
     });
 
-    console.log("[OMNILINK] Resposta:", data.substring(0, 1000));
+    console.log("[OMNILINK] ObtemEventosCtrl resposta:", data.substring(0, 1000));
 
   } catch (err) {
     console.error("[OMNILINK] Erro:", err.message);
@@ -47,5 +72,3 @@ async function syncOmnilink() {
 }
 
 module.exports = { syncOmnilink };
-
-
